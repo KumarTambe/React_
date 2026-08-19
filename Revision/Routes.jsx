c0db57b656ad43180bb391dae6f2805f67d8a227
Kumar Tambe <kumartambe49@gmail.com>
2026-08-19 23:52:33 +0530

    day 4 - routes revised agian

--- Revision/Routes.jsx
+++ Revision/Routes.jsx
@@ -1,5 +1,39 @@
-import { BrowserRoutes, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom'
 
 function HomePage() {
-    const useNavigate = useNavigate();
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1>Are aee home page</h1>
+            <Link to='/about'> Go to About page</Link>
+            <Link to='/user/42'> Go to user profile</Link>
+        </>
+    )
+}
+
+function About() {
+    return <h1>About Page</h1>
+}
+
+function UserProfile() {
+    const { id } = useParams()
+    return <h1>User Profile — ID: {id}</h1>
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to='/about'>Go to about page</Link>
+                <Link to='/user/42'>Go to user 42</Link>
+            </nav>
+
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:id' element={<UserProfile />} />
+            </Routes>
+
+        </BrowserRouter>
+    )
 }
