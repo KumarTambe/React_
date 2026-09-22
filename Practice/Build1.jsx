a32f308097e01d6788a7e3ca8a9e4c02521c208e
Kumar Tambe <kumartambe49@gmail.com>
2026-09-22 23:17:26 +0530

    react practice

--- Practice/Build1.jsx
+++ Practice/Build1.jsx
@@ -28,3 +28,54 @@ function JobList({ jobs }) {
 function App() {
     return <JobList jobs={jobs} />
 }
+
+//exercise 3 
+
+function StudentBadge({ name, cgpa }) {
+    return (
+        <>
+            <h1>{cgpa > 8 ? `Top Student ${name}` : cgpa < 6 ? "Low CGPA" + name : name}</h1>
+        </>
+    )
+}
+
+//practice
+
+function ProfileCard({ name, role, skills, isAvailable }) {
+    return (
+        <>
+            <h1> {name} is a {role} and has the following skils :</h1>
+            <>
+                {skills.map((s) =>
+                    <span key={index}>{s}</span>
+                )}
+            </>
+            <h1>{isAvailable ? "Open to work" : "Not avaiable"}</h1>
+        </>
+    )
+}
+
+function App() {
+    return (
+        <div>
+            <ProfileCard
+                name="Kumar"
+                role="Developer"
+                skills={["React", "Node", "MongoDB"]}
+                isAvailable={true}
+            />
+            <ProfileCard
+                name="Prasad"
+                role="Designer"
+                skills={["Figma", "CSS"]}
+                isAvailable={false}
+            />
+            <ProfileCard
+                name="Tanmay"
+                role="Backend Dev"
+                skills={["Node", "PostgreSQL"]}
+                isAvailable={true}
+            />
+        </div>
+    )
+}
